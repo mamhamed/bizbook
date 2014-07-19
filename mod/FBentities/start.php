28cd389ce9a9d7d0334d5061415a1b00046cef79
mamhamed <mhfirooz@gmail.com>
2014-07-19 06:09:40 +0000

    supporting likes and friends page

---
+++ mod/FBentities/start.php
@@ -0,0 +1,17 @@
+<?php
+elgg_register_event_handler('init', 'system', 'FBentities_init');
+
+function FBentities_init(){
+   elgg_register_page_handler('FBentities','FBentities_page_handler');
+}
+
+function FBentities_page_handler($segments){
+  elgg_load_library('facebook');
+  if ($segments[0] == "likes"){
+     include elgg_get_plugins_path() . 'FBentities/pages/FBentities/Likes.php';
+  }
+  else{
+     include elgg_get_plugins_path() . 'FBentities/pages/FBentities/viewFBfriends.php';
+  }
+  true;
+}
