64943f84485ae0c92e6447aa0c96f4f6a225db44
mamhamed <mhfirooz@gmail.com>
2014-07-19 06:20:51 +0000

    add viewFBlikes page

--- mod/FBentities/start.php
+++ mod/FBentities/start.php
@@ -8,7 +8,7 @@ function FBentities_init(){
 function FBentities_page_handler($segments){
   elgg_load_library('facebook');
   if ($segments[0] == "likes"){
-     include elgg_get_plugins_path() . 'FBentities/pages/FBentities/Likes.php';
+     include elgg_get_plugins_path() . 'FBentities/pages/FBentities/viewFBlikes.php';
   }
   else{
      include elgg_get_plugins_path() . 'FBentities/pages/FBentities/viewFBfriends.php';
