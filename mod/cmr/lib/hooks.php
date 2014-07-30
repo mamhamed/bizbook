414a235a229113ad268fd651308d0a45da388222
shahdad <shahdad.irajpour@gmail.com>
2014-07-29 23:39:03 -0700

    Fixing three bugs in implementation of cmr: 1) action/fr_approve link had a typo 2) An option of 'Add friend' would be given to a client against a biz 3) An option of 'Add friend' was given to a biz against a client as well as another biz.

--- mod/cmr/lib/hooks.php
+++ mod/cmr/lib/hooks.php
@@ -48,21 +48,34 @@ function cmr_user_hover_menu($hook, $type, $return, $params) {
 			}
 			$item->setSection('action');
 			$return[] = $item;
-				
-			// If c2b relationship exists, include 'Revoke as a service provider' option in the menu. Else, include 'Select as a service provider' option in the menu.
-			if (check_entity_relationship($user->getGUID(), 'c2b', $target_user->guid)) {
-				$url = elgg_add_action_tokens_to_url("action/c2b_revoke?object_guid=$target_user->guid");
-				$item = new ElggMenuItem('c2b_revoke', elgg_echo('cmr:c2b_revoke:user'), $url);
-			} else {
-				$url = elgg_add_action_tokens_to_url("action/c2b_establish?object_guid=$target_user->guid");
-				$item = new ElggMenuItem('c2b_establish', elgg_echo('cmr:c2b_establish:user'), $url);
-			}
-			$item->setSection('action');
-			$return[] = $item;
 
+			// Replace the default 'Add friend' option in the menu with 'Revoke as a service provider', if c2b relationship exists. Replace it with 'Select as a service provider' otherwise.  
+			foreach ($return as &$item) {
+				if($item->getName() == "add_friend") {
+					if (check_entity_relationship($user->getGUID(), 'c2b', $target_user->guid)) {
+						$item->setText(elgg_echo("cmr:c2b_revoke:user"));
+						$item->setHref("action/c2b_revoke?object_guid=$target_user->guid");
+						break;
+					} else {
+						$item->setText(elgg_echo("cmr:c2b_establish:user"));
+						$item->setHref("action/c2b_establish?object_guid=$target_user->guid");
+						break;
+					}
+				}
+			}
+				
 		} elseif (elgg_instanceof($user, "user", "business_subtype") && elgg_instanceof($target_user, "user", "client_subtype")) {
 			// Current user: business
 			// target_user: client
+
+			// Delete the default 'Add friend' option in the menu.
+			$old_return = $return;
+			$return = array();
+			for ($i = 0; $i < count($old_return); ++$i) {
+				if ($old_return[$i]->getName() != "add_friend") {
+					$return[] = $old_return[$i];
+				}
+			}
 
 			// If flw relationship exists, include 'Drop as a follower' option in the menu.
 			if (check_entity_relationship($target_user->guid, 'flw', $user->getGUID())) {
@@ -79,7 +92,21 @@ function cmr_user_hover_menu($hook, $type, $return, $params) {
 				$item->setSection('action');
 				$return[] = $item;
 			}
+
+		} elseif (elgg_instanceof($user, "user", "business_subtype") && elgg_instanceof($target_user, "user", "business_subtype")) {
+			// Current user: business
+			// target_user: business
+		
+			// Delete the default 'Add friend' option in the menu.
+			$old_return = $return;
+			$return = array();
+			for ($i = 0; $i < count($old_return); ++$i) {
+				if ($old_return[$i]->getName() != "add_friend") {
+					$return[] = $old_return[$i];
+				}
+			}		
 		}
+		
 	}
 
 	return $return;
