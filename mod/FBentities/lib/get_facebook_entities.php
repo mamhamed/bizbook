cda7fd97b85788f499519915f512d660141036f6
Hamed Firooz <mhfirooz@gmail.com>
2014-08-01 00:24:16 -0700

    add library to get facebook likes and places

---
+++ mod/FBentities/lib/get_facebook_entities.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hamed
+ * Date: 8/1/14
+ * Time: 12:01 AM
+ */
+
+function get_facebook_places($places, $facebook){
+    $category_place = array();
+    foreach ($places["data"] as $value) {
+        $pageid = $value["place"]["id"];
+        $myplace = $facebook->api($pageid);
+        $ppic = $facebook->api($pageid.'/picture?redirect=false');
+        $imgsrc = $ppic["data"]["url"];
+
+        $allcategory = $myplace["category_list"];
+        //add category
+        foreach ($allcategory as $categorya){
+            $category = $categorya["name"];
+            $obj = array('name' => $myplace["name"], 'imgsrc' => $imgsrc, 'fbid' => $myplace["id"]);
+            if (!isset($category_place[$category])){
+                $category_place[$category] = array($obj);
+            }else{
+                array_push($category_place[$category], $obj);
+            }
+        }
+    }
+
+    return $category_place;
+
+}
+
+function get_facebook_likes($likes){
+    $category_like = array();
+    foreach ($likes["data"] as $value) {
+        $pageid = $value["id"];
+        $pagename = $value["name"];
+
+        //create biz page by fb info
+        $page_register_name = create_fb_biz_profile($pageid, $pagename);
+
+        $imgsrc = 'https://graph.facebook.com/' . $value["id"] . '/picture';
+
+        $category = $value["category"];
+
+        $obj = array('name' => $pagename, 'imgsrc' => $imgsrc, 'fbid' => $pageid, 'elggAddress' => 'http://localhost/elgg/profile/'.$page_register_name);
+        if (!isset($category_like[$category])){
+            $category_like[$category] = array($obj);
+        }else{
+            array_push($category_like[$category], $obj);
+
+        }
+    }
+
+    return $category_like;
+}
+
+function create_fb_biz_profile($pageid, $pagename){
+    $user = elgg_get_logged_in_user_entity();
+    $page_register_name = str_replace(' ', '', $pagename);
+    if (ctype_alnum($page_register_name)){
+        $page_user = get_user_by_username($page_register_name);
+        if ($page_user == null){
+            $pageGUID = register_user($page_register_name, '1234567', $pagename, $pageid . '@facebook.com' , "business_profile_type");
+            $user->addFriend($pageGUID);
+            $page_profile_pic = 'https://graph.facebook.com/' . $pageid .'/picture?type=large';
+            facebook_connect_update_user_avatar($page_user, $page_profile_pic);
+        }
+    }
+
+    return $page_register_name;
+}
